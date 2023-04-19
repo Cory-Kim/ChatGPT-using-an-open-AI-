fcaea1c536b88971e0560099555a071ef67b1f54
LAPTOP-23C0I0R6\cdoky <cdokyung@gmail.com>
2023-04-19 03:13:18 -0700

    set up basic server

---
+++ server/index.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import env from 'dotenv';
+import { Configuration, OpenAIApi } from 'openai';
+
+const app = express();
+
+env.config();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+// Configure open api
+const configuration = new Configuration({
+  organization: "org-ZBSvugSeJC2AWklw6VZnEetR",
+  apiKey: process.env.API_KEY
+});
+
+const openai = new OpenAIApi(Configuration);
+
+// Listening
+app.listen("3080", () => console.log("Listening on port 3080"));
+
+// Dummy route to test
+app.get("/", (req, res) =>
+{
+  res.send("Hello World!");
+})
+
+// Post route for making request
+app.post('/', async (req, res) =>
+{
+  const { message } = req.body
+  
+  try {
+    const response = await openai.createCompletion({
+      model: "text-devinci-003",
+      prompt: `${message}`,
+      max_tokens: 100,
+      temperature: .5
+    });
+    res.json({ message: response.data.choices[0].value });
+
+  } catch (error) {
+    console.log(error);
+    res.send(error).status(400);
+  }
+})
