a5e0423ed3fddab65abe74e6c324458cf7aa6b94
LAPTOP-23C0I0R6\cdoky <cdokyung@gmail.com>
2023-04-18 21:25:22 -0700

    Started working on ChatGPT project

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App()
+{
+
+
+  return (
+    <div className='bg-[#1A232E] h-screen py-6 relative sm:px-28 text-white overflow-hidden flex flex-col justify-between align-middle' >
+      {/* Header */}
+      <div className='uppercase font-bold text-2xl'>ChatGPT</div>
+
+      {/* Body */}
+      <div>body</div>
+
+      {/* Input */}
+      <div>input</div>
+    </div>
+  )
+}
+
+export default App
