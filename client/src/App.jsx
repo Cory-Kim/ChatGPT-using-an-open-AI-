96cb33419d2b363ea5848d67f97124ad37de22e0
LAPTOP-23C0I0R6\cdoky <cdokyung@gmail.com>
2023-04-19 02:46:58 -0700

    finished front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css';
 import ChatBody from './components/ChatBody';
+import ChatInput from './components/ChatInput';
 
 function App()
 {
 
 
   return (
-    <div className='bg-[#1A232E] h-screen py-6 relative sm:px-28 text-white overflow-hidden flex flex-col justify-between align-middle' >
+    <div className='bg-[#071526] h-screen py-6 relative sm:px-16 px-12 text-white overflow-hidden flex flex-col justify-between align-middle' >
       {/* gradient */}
       <div className='gradient-01 z-0 absolute'></div>
       <div className='gradient-02 z-0 absolute'></div>
@@ -15,12 +15,14 @@ function App()
       <div className='uppercase font-bold text-2xl text-center mb-3'>ChatGPT</div>
 
       {/* Body */}
-      <div>
+      <div className='h-[90%] overflow w-full max-w-4xl min-w-[20rem] py-8 self-center px-4'>
         <ChatBody />
       </div>
 
       {/* Input */}
-      <div>input</div>
+      <div className='w-full max-w-4xl min-w-[20rem] self-center'>
+        <ChatInput />
+      </div>
     </div>
   )
 }
