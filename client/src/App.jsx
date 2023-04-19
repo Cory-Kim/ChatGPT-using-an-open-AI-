9580aaf8b72688573041953a0b1888228e4b2871
LAPTOP-23C0I0R6\cdoky <cdokyung@gmail.com>
2023-04-18 22:11:38 -0700

    added some CSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import ChatBody from './components/ChatBody';
 
 function App()
 {
@@ -6,11 +7,17 @@ function App()
 
   return (
     <div className='bg-[#1A232E] h-screen py-6 relative sm:px-28 text-white overflow-hidden flex flex-col justify-between align-middle' >
+      {/* gradient */}
+      <div className='gradient-01 z-0 absolute'></div>
+      <div className='gradient-02 z-0 absolute'></div>
+
       {/* Header */}
-      <div className='uppercase font-bold text-2xl'>ChatGPT</div>
+      <div className='uppercase font-bold text-2xl text-center mb-3'>ChatGPT</div>
 
       {/* Body */}
-      <div>body</div>
+      <div>
+        <ChatBody />
+      </div>
 
       {/* Input */}
       <div>input</div>
